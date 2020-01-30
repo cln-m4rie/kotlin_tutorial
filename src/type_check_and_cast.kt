4d40fbb7eb33fd6e7b06afa32d2aed36a62a9afa
cln-m4rie <s.kikkawa0613@gmail.com>
2020-01-30 13:54:21 +0900

    add

--- src/type_check_and_cast.kt
+++ src/type_check_and_cast.kt
@@ -1,4 +1,4 @@
-fun getStringLength(obj: Any): Int? {
+fun getStringLength1(obj: Any): Int? {
     // isを使う事で型キャストができる。この場合はStringへキャストしている
     if (obj is String) {
         // `obj` is automatically cast to `String` in this branch
@@ -9,12 +9,20 @@ fun getStringLength(obj: Any): Int? {
     return null
 }
 
+fun getStringLength2(obj: Any): Int? {
+    // 先にString型か確認するやり方
+    if (obj !is String) return null
+
+    // `obj` is automatically cast to `String` in this branch
+    return obj.length
+}
+
 
 fun main() {
     fun printLength(obj: Any) {
         // "?:" なんか三項演算子っぽいやつ。エルビス演算子っていうらしい
         // getStringLengthの値があればそれを返し、偽であれば右辺の値を返す
-        println("'$obj' string length is ${getStringLength(obj) ?: "... err, not a string"} ")
+        println("'$obj' string length is ${getStringLength1(obj) ?: "... err, not a string"} ")
     }
     printLength("Incomprehensibilities")
     printLength(1000)
